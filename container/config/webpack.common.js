9f58d385224e45281ed8170f3bca1f1eede0b505
Ivan Derlich <a@ivanderlich.com>
2026-01-20 17:20:10 -0300

    Make container app work in isolation

---
+++ container/config/webpack.common.js
@@ -0,0 +1,21 @@
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+
+    ],
+  },
+};
