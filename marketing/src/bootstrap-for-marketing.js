f3639c38dc7e9824f0aaeddbd58bf012e8c65c31
Ivan Derlich <a@ivanderlich.com>
2026-01-21 15:03:56 -0300

    Change file name to add specificity

--- marketing/src/bootstrap-for-marketing.js
+++ marketing/src/bootstrap-for-marketing.js
@@ -9,12 +9,13 @@ const mount = (el) => {
 
 if (window.__MICRO_FRONTEND_CONTAINER__) {
   console.log(
-    "Marketing App running as a micro-frontend application integrated to a container."
+    "-- Marketing App running as a micro-frontend application integrated to a container.",
   );
 } else {
-  console.log("Marketing App running in isolation. Mounting immediately.");
+  console.log("-- Marketing App running in isolation. Mounting immediately.");
   const el = document.querySelector("#_marketing-dev-root");
   if (!el) throw new Error("No root element found to mount the Marketing app.");
+  console.log("-- mount: ", mount);
 
   mount(el);
 }
